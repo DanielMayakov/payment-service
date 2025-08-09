d4adbc8c81586765e8ae4e499fc7f9ec2da631b7
danil <danil.mayakov@yandex.ru>
2025-08-09 19:24:47 +0300

    сделал урок 4

--- payment-service-app/src/main/java/com/iprody/payment/service/app/model/Payment.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/model/Payment.java
@@ -2,9 +2,11 @@ package com.iprody.payment.service.app.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
     private Long id;
     private String description;
