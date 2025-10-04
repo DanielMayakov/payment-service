0670413a8d3fd02d0ff7c0381d141fc8f235a45f
danil <danil.mayakov@yandex.ru>
2025-10-04 16:32:11 +0300

    сделал урок 13

--- payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/controller/PaymentController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,22 +30,26 @@ public class PaymentController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('admin')")
     @ResponseStatus(HttpStatus.CREATED)
     public PaymentDto create(@RequestBody @Valid PaymentDto dto) { // добавили @Valid
         return paymentService.create(dto);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('admin', 'reader')")
     public PaymentDto get(@PathVariable UUID id) {
         return paymentService.get(id);
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('admin', 'reader', 'user')")
     public Page<PaymentDto> search(@ModelAttribute PaymentFilter filter, Pageable pageable) {
         return paymentService.searchPagedDto(filter, pageable);
     }
 
     @GetMapping("/search")
+    @PreAuthorize("hasAnyRole('admin', 'reader')")
     public Page<PaymentDto> searchPayments(
             @ModelAttribute PaymentFilter filter,
             @RequestParam(defaultValue = "0") @Min(0) int page,
@@ -63,11 +68,13 @@ public class PaymentController {
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('admin', 'reader')")
     public PaymentDto update(@PathVariable UUID id, @RequestBody @Valid PaymentDto dto) { // добавили @Valid
         return paymentService.update(id, dto);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('admin')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable UUID id) {
         paymentService.delete(id);
